2e0116e53f7ce4939fc5cc60965b0b77609aff12
Igor Shahin <igor.shahin@icloud.com>
2025-08-25 15:19:18 +0300

    add first task

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,4 +1,18 @@
 package ru.netology;
 
+import ru.netology.service.YearService;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        YearService service = new YearService();
+
+        System.out.println("Введите год в формате yyyy");
+        int year = scanner.nextInt();
+
+        int days = service.getDaysInYear(year);
+        System.out.println("Количество дней " + days);
+    }
 }
---
+++ src/main/java/ru/netology/service/YearService.java
@@ -0,0 +1,15 @@
+package ru.netology.service;
+
+public class YearService {
+    public int getDaysInYear(int year) {
+        if (year % 400 == 0) {
+            return 366;
+        } else if (year % 100 == 0) {
+            return 365;
+        } else if (year % 4 == 0) {
+            return 366;
+        } else {
+            return 365;
+        }
+    }
+}
